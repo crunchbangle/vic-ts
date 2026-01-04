2c51540f7f2ad9ace76335e98f9117fdb618a800
Crunch Bangle <crunchbangle@tuta.io>
2026-01-04 11:27:59 +0000

    refactored key generator

--- generateKey.test.ts
+++ generateKey.test.ts
@@ -1,96 +1,34 @@
-import { chainAdd } from "./chainAddition";
-import { columnarTransposeRows, getSequence } from "./columnarTransposition";
-import { extractLastTwoUnique } from "./extractLastTwoUnique";
-import { noCarryAddString, noCarrySubtractString } from "./noCarryAddition";
+import { encode, getKeyGenerator } from "./generateKey";
 
 const pin = 6;
 const date = '1391959';
 const phrase = 'twas the night before christmas';
 const keyGroup = '72401';
 
-
-const encode = (key:string, aide:string, text:string) => {
-    let result = '';
-    for(let i=0; i<text.length; i++){
-        result += key[aide.indexOf(text[i])];
-    }
-    return result;
-}
-
 test('encoding', ()=>{
     expect(encode('6013589427', '1234567890', '90210'))
         .toBe('27067');
 })
 
-
-export const generateKey = (pin:number, date:string, phrase:string, keyGroup:string) => {
-    const truncDate = date.substr(0, 5);
-    const keyInsertion = parseInt(date.substr(5,1));
-    const lineC = noCarrySubtractString(keyGroup, truncDate);
-    const lineD = phrase.toUpperCase()
-        .replace(/ /g,'')
-        .substr(0, 20)
-        .replace(/^(.{10})/, '$1 ');
-    const lineE = getSequence(lineD.substr(0,10)) +
-        ' ' + getSequence(lineD.substr(11,10));
-    const lineF = lineC+chainAdd(lineC, 5) 
-        + ' 1234567890';
-    const lineG = noCarryAddString(lineE.substr(0,10),lineF.substr(0,10))
-    const lineH = encode(lineE.substr(11,10), lineF.substr(11,10), lineG);
-    const lineJ = getSequence(lineH); // columnar key for the next rows
-    const lineK = chainAdd(lineH, 10);
-    const lineL = chainAdd(lineK, 10);
-    const lineM = chainAdd(lineL, 10);
-    const lineN = chainAdd(lineM, 10);
-    const lineP = chainAdd(lineN, 10);
-    const ltned = extractLastTwoUnique(lineP);
-    const a = pin + parseInt(ltned[0]);
-    const b = pin + parseInt(ltned[1]);
-
-    const rows = [lineK, lineL, lineM, lineN, lineP];
-    const qr = columnarTransposeRows(lineJ, rows).join('');
-
-    const lineQ = qr.substr(0,a); // key to columnar
-    const lineR = qr.substr(a,b); // key to diagonal
-
-    const lineS = getSequence(lineP); // key to straddling checkerboard
-
-    return `${lineC}
-${lineD}
-${lineE}
-${lineF}
-${lineG}
-${lineH}
-${lineJ}
-${lineK}
-${lineL}
-${lineM}
-${lineN}
-${lineP}
-${lineQ}
-${lineR}
-${lineS}
-`;
-}
-
-const expected = `69592
-TWASTHENIG HTBEFORECH
-8017942653 6013589427
-6959254417 1234567890
-4966196060
-3288628787
-3178429506
-5064805552
-5602850077
-1620350748
-7823857125
-5051328370
-0668005552551
-758838
-5961328470
-`;
-
-test('update this', () => {
-    expect(generateKey(pin, date, phrase, keyGroup)).toBe(expected);
+test('Key Generator', () => {
+    const kg = getKeyGenerator(pin, date, phrase, keyGroup);
+    expect(kg.keyInsertion).toBe(5);
+    expect(kg.lineA).toBe('72401');
+    expect(kg.lineB).toBe('13919');
+    expect(kg.lineC).toBe('69592');
+    expect(kg.lineD).toBe('TWASTHENIG HTBEFORECH');
+    expect(kg.lineE).toBe('8017942653 6013589427');
+    expect(kg.lineF).toBe('6959254417 1234567890');
+    expect(kg.lineG).toBe('4966196060');
+    expect(kg.lineH).toBe('3288628787');
+    expect(kg.lineJ).toBe('3178429506');
+    expect(kg.lineK).toBe('5064805552');
+    expect(kg.lineL).toBe('5602850077');
+    expect(kg.lineM).toBe('1620350748');
+    expect(kg.lineN).toBe('7823857125');
+    expect(kg.lineP).toBe('5051328370');
+    expect(kg.lineQ).toBe('0668005552551');
+    expect(kg.lineR).toBe('758838');
+    expect(kg.lineS).toBe('5961328470');
 })
 
--- generateKey.ts
+++ generateKey.ts
@@ -11,3 +11,83 @@ J H sequenced
 K-P chain addition of H for 50 digits
 */
 
+import { chainAdd } from "./chainAddition";
+import { columnarTransposeRows, getSequence } from "./columnarTransposition";
+import { extractLastTwoUnique } from "./extractLastTwoUnique";
+import { noCarryAddString, noCarrySubtractString } from "./noCarryAddition";
+
+class KeyGenerator {
+    personalNumber: number;
+    date: string; // we'll keep the whole thing here
+    phrase: string; // we'll keep the whole thing here
+    keyInsertion: number; // 6th digit of date
+    lineA: string; // keygroup
+    lineB: string; // 5 digits of date
+    lineC: string;
+    lineD: string;
+    lineE: string;
+    lineF: string;
+    lineG: string;
+    lineH: string;
+    lineJ: string;
+    lineK: string;
+    lineL: string;
+    lineM: string;
+    lineN: string;
+    lineP: string;
+    lineQ: string;
+    lineR: string;
+    lineS: string;
+
+    constructor(pin:number, date:string, phrase:string, keyGroup:string){
+
+        this.personalNumber = pin;
+        this.date = date;
+        this.phrase = phrase;
+        this.lineA = keyGroup;
+
+        this.lineB = date.substr(0, 5);
+        this.keyInsertion = parseInt(date.substr(5,1));
+        this.lineC = noCarrySubtractString(keyGroup, this.lineB);
+        this.lineD = phrase.toUpperCase()
+            .replace(/ /g,'')
+            .substr(0, 20)
+            .replace(/^(.{10})/, '$1 ');
+        this.lineE = getSequence(this.lineD.substr(0,10)) +
+            ' ' + getSequence(this.lineD.substr(11,10));
+        this.lineF = this.lineC + chainAdd(this.lineC, 5) 
+            + ' 1234567890';
+        this.lineG = noCarryAddString(this.lineE.substr(0,10), this.lineF.substr(0,10))
+        this.lineH = encode(this.lineE.substr(11,10), this.lineF.substr(11,10), this.lineG);
+        this.lineJ = getSequence(this.lineH); // columnar key for the next rows
+        this.lineK = chainAdd(this.lineH, 10);
+        this.lineL = chainAdd(this.lineK, 10);
+        this.lineM = chainAdd(this.lineL, 10);
+        this.lineN = chainAdd(this.lineM, 10);
+        this.lineP = chainAdd(this.lineN, 10);
+        const ltned = extractLastTwoUnique(this.lineP);
+        const a = pin + parseInt(ltned[0]);
+        const b = pin + parseInt(ltned[1]);
+
+        const rows = [this.lineK, this.lineL, this.lineM, this.lineN, this.lineP];
+        const qr = columnarTransposeRows(this.lineJ, rows).join('');
+
+        this.lineQ = qr.substr(0,a); // key to columnar
+        this.lineR = qr.substr(a,b); // key to diagonal
+
+        this.lineS = getSequence(this.lineP); // key to straddling checkerboard
+
+    }
+}
+
+export const encode = (key:string, aide:string, text:string) => {
+    let result = '';
+    for(let i=0; i<text.length; i++){
+        result += key[aide.indexOf(text[i])];
+    }
+    return result;
+}
+
+export const getKeyGenerator = (pin:number, date:string, phrase:string, keyGroup:string) => {
+    return new KeyGenerator(pin, date, phrase, keyGroup);
+}
